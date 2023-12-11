8ee216084fa2517184683b6fd77c733de88802cf
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-12-11 12:21:02 +0600

    added google poppins font using tailwind theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
 
   return (
-    <>
+    <div className="font-poppins">
       <h1 className="text-4xl text-center">Welcome to my portfolio website.</h1>
-    </>
+    </div>
   )
 }
 
