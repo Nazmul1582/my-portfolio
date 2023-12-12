4ee8a1c0be295322d8ab18bc9af92fdc7c171a9f
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-12-12 15:20:18 +0600

    added some services data into index.js file

--- src/index.js
+++ src/index.js
@@ -23,4 +23,38 @@ export const navLinks = [
       id: "contact",
       title: "Contact",
     },
-  ];
+];
+
+
+export const services = [
+  {
+    id: 1,
+    title: "MERN Stack Development",
+    description: "I specialize in building dynamic web applications using the MERN (MongoDB, Express.js, React.js, Node.js) stack. From database design to frontend user interfaces, I ensure seamless integration across the entire stack."
+  },
+  {
+    id: 2,
+    title: "Web Development",
+    description: "Crafting responsive and interactive websites is my expertise. I focus on creating web solutions that not only meet the client's requirements but also provide a great user experience."
+  },
+  {
+    id: 3,
+    title: "Front End Development",
+    description: "Transforming design concepts into intuitive and engaging user interfaces is what I do best. I utilize modern frontend technologies like HTML, CSS, and JavaScript frameworks to bring ideas to life."
+  },
+  {
+    id: 4,
+    title: "Back End Development",
+    description: "Implementing robust server-side logic and APIs is my forte. I have hands-on experience with backend technologies like Node.js, Express.js, and database management systems to ensure efficient data processing."
+  },
+  {
+    id: 5,
+    title: "Full Stack Development",
+    description: "As a full-stack developer, I am well-versed in both frontend and backend technologies. From conceptualization to deployment, I can handle the entire development process, ensuring a cohesive and integrated solution."
+  },
+  {
+    id: 6,
+    title: "Responsive Web Design",
+    description: "Creating websites that seamlessly adapt to various devices and screen sizes is essential in today's digital landscape. I specialize in responsive web design, ensuring a consistent and optimal user experience across all platforms."
+  }
+];
